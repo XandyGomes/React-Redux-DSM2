bde4de3b63c6f6b3a33ea353fa3b758cae5ee891
XandyGomes <x3.computadores@hotmail.com>
2022-11-17 22:12:09 -0300

    Aula.17.11.2022

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ function App() {
       <h1>Projeto React-Redux</h1>
 
       <div className='linha'>
-        <Card> </Card>
+        <Card title="Card 1" red>TESTE 1</Card>
       </div>
 
       <div className='linha'>
-        <Card> </Card>
-        <Card> </Card>
-        <Card> </Card>
+        <Card title="Card 2" green>TESTE 2</Card>
+        <Card title="Card 3" blue>TESTE 3</Card>
+        <Card title="Card 4" purple>TESTE 4</Card>
       </div>
+
 
     </div>
   );
